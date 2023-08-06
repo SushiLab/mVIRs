7a1c268709b4b756feffbe001b451c9567eda527
valentynbez <walentyn.bzsz@gmail.com>
2023-08-06 18:50:08 +0200

    feat: `get_paired_alignments` function

--- mVIRs/alignment_processing.py
+++ mVIRs/alignment_processing.py
@@ -3,6 +3,11 @@ from typing import Dict
 
 from .containers import MappedRead, Mapping
 
+from collections import namedtuple, defaultdict
+from itertools import product
+PAlignment = namedtuple('PAlignment',
+                        'iss ref revr1 revr2 score startr1 endr1 startr2 endr2 orientation')
+
 
 def mapped_reads_from_alignment(bam_file : AlignmentFile,
                                 min_coverage : float = 0,
@@ -34,7 +39,7 @@ def mapped_reads_from_alignment(bam_file : AlignmentFile,
 
         if readname not in mapped:
             mapped[readname] = MappedRead(name=readname)
-        mapped[readname][orientation] = Mapping(alignment, extended=extended_info)
+        mapped[readname][orientation] = Mapping.from_aligned_segment(alignment, extended=extended_info)
 
     alignments.close()
 
@@ -81,9 +86,60 @@ def get_paired_alignments(read_mappings: Dict[str, MappedRead],
     cnt_singleend = 0
     cnt_pairedend = 0
 
-    for query, alignments in read_mappings:
+    for query, alignments in read_mappings.items():
         if alignments.is_single_end():
             cnt_singleend += 1
             continue
         else:
             cnt_pairedend += 1
+            score_filtered = {}
+
+            matches = {}
+            samematches = []
+            bestscore = 0
+            matches = defaultdict(list)
+            cutoff_bestscore = 0.95
+
+            for alnr1, alnr2 in product(alignments['R1'],
+                                        alignments['R2']):
+                if alnr1.ref != alnr2.ref:  # if both reads align to 2 different references we continue
+                    continue
+                orientation: str = _calculate_orientation(alnr1.rev, alnr1.rstart, alnr2.rev,  alnr2.rstart)
+                positions = [alnr1.rstart, alnr2.rstart, alnr1.rend, alnr2.rend]
+                iss = abs(max(positions) - min(positions))
+                score = alnr1.score + alnr2.score
+                # add SAME oritentation to a separate container
+                if orientation == 'SAME':
+                    samematches.append(
+                        PAlignment(iss=iss, revr1=alnr1.rev, revr2=alnr2.rev,
+                                score=score, startr1=alnr1.rstart, endr1=alnr1.rend, startr2=alnr2.rstart, endr2=alnr2.rend,
+                                orientation=orientation, ref=alnr1.ref))
+
+                else:
+                    if bestscore < score:
+                        bestscore = score
+                    matches[alnr1.score + alnr2.score].append(
+                        PAlignment(iss=iss, revr1=alnr1.rev, revr2=alnr2.rev, score=score,
+                                    startr1=alnr1.rstart, endr1=alnr1.rend, startr2=alnr2.rstart,
+                                    endr2=alnr2.rend, orientation=orientation, ref=alnr1.ref))
+
+            minscore: int = int(bestscore * cutoff_bestscore)
+            scoreinsertsizesortedmatches = []
+            for score in sorted(matches.keys(), reverse=True):
+                if score < minscore:
+                    break
+                match = matches[score]
+                tmpmatch = sorted(match, key=lambda x: x.iss)
+                scoreinsertsizesortedmatches = scoreinsertsizesortedmatches + tmpmatch
+
+            for samematch in samematches:
+                print(samematch)
+                if samematch.score >= minscore:
+                    scoreinsertsizesortedmatches.append(samematch)
+
+            if len(scoreinsertsizesortedmatches) == 0:
+                continue
+
+            score_filtered[query] = scoreinsertsizesortedmatches
+
+            return score_filtered
--- tests/test_alignment_processing.py
+++ tests/test_alignment_processing.py
@@ -1,8 +1,11 @@
 import unittest
 
+from mVIRs.containers import MappedRead, Mapping
 from mVIRs.alignment_processing import (
     mapped_reads_from_alignment,
-    _calculate_orientation
+    _calculate_orientation,
+    PAlignment,
+    get_paired_alignments
 )
 
 class TestMappedReadsFromAlignment(unittest.TestCase):
@@ -26,11 +29,24 @@ class TestCalculateOrientation(unittest.TestCase):
         self.paired_end_1 = (True, 5, False, 1)
         self.paired_end_2 = (False, 1, True, 5)
 
-
-
     def test_calculate_orientation(self):
         self.assertEqual(_calculate_orientation(*self.same), "SAME")
         self.assertEqual(_calculate_orientation(*self.opr_reverse_r1), "OPR")
         self.assertEqual(_calculate_orientation(*self.opr_reverse_r2), "OPR")
         self.assertEqual(_calculate_orientation(*self.paired_end_1), "PAIREDEND")
         self.assertEqual(_calculate_orientation(*self.paired_end_2), "PAIREDEND")
+
+class TestGetPairedReads(unittest.TestCase):
+    def setUp(self) -> None:
+        self.read_name = 'ERR4552622.32'
+        self.test_case = MappedRead(name=self.read_name)
+        self.test_case["R1"] = Mapping(False, "SalmonellaLT2", 0, 151,
+                                       151, [(0, 151)], [(0, 151)])
+
+        self.test_case["R2"] = Mapping(True, "SalmonellaLT2", 2399129, 2399276,
+                                       147, [(0, 147)], [(2399129, 2399276)])
+
+    def test_get_paired_reads(self):
+        pe_aln = get_paired_alignments({self.read_name: self.test_case})[self.read_name]
+        self.assertEqual(pe_aln[0].iss, 2399276)
+        self.assertEqual(pe_aln[0].orientation, 'PAIREDEND')
