987282d8c9712ed4ec9df2c8aee8dd8ce33a051b
valentynbez <walentyn.bzsz@gmail.com>
2023-08-09 18:58:58 +0200

    fix: readabe utils

--- mVIRs/utils.py
+++ mVIRs/utils.py
@@ -20,9 +20,13 @@ def check_sequences(r1_filepath, r2_filepath, n_headers=250):
 
     r1_fasta, r2_fasta = FastxFile(r1_filepath), FastxFile(r2_filepath)
     counter = 0
-    for f_entry, r_entry in zip(r1_fasta, r2_fasta):
-        if f_entry.name != r_entry.name:
-            logging.error(f'Names of input reads do not match. ({h1} != {h2}). Check if read files belong together. Quitting')
+    for r1_entry, r2_entry in zip(r1_fasta, r2_fasta):
+        name_1 = r1_entry.name
+        name_2 = r2_entry.name
+        if name_1 != name_2:
+            logging.error('Names of input reads do not match. ' \
+                          f'({name_1} != {name_2}). Check if read ' \
+                          'files belong together. Quitting.')
             shutdown(1)
         counter += 1
         if counter == n_headers:
